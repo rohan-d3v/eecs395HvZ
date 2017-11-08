db9fe0ce0e7233d014ab7c41a2266cfc756c180a
Ubuntu <ubuntu@ip-172-31-25-136.us-west-2.compute.internal>
2017-11-08 18:22:03 +0000

    delete cloned folder

--- cwru-hvz-source/app/serializers/api/v1/mission_serializer.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::MissionSerializer < Api::V1::BaseSerializer
-  attributes :id, :created_at, :updated_at,
-             :game_id, :start, :end, :description, :winning_faction_id,
-             :title, :storyline
-
-  #belongs_to :game
-  has_many :attendances
-  has_many :feeds
-end
